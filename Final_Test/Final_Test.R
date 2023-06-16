52c6bdd6cf8c750e232285fb2842eb1f1058bccc
qqaazz0222 <qqaazz0222@naver.com>
2023-06-16 23:43:15 +0900

    추가 : R(CSV 읽어오기)

--- Final_Test/Final_Test.R
+++ Final_Test/Final_Test.R
@@ -8,25 +8,10 @@ library(rstudioapi)
 library(dplyr)
 library(readr)
 library(ggplot2)
-library(gridExtra)
 # ---------------------------------------------------------------------------------------------------------------------------------
 
 dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
 
 # ---------------------------------------------------------------------------------------------------------------------------------
 
-data <- read_csv(paste(dir, "/eat-R.csv", sep = ""))
-
-
-ratioEat <- data %>%
-  ggplot(aes(x=week, fill=iseat)) +
-  geom_bar(position='stack')
-
-groupData <- data %>% group_by(week) %>% count(type)
-
-typeG <- groupData %>%
-  ggplot(aes(x=week, y=n, fill=type))+
-  geom_bar(stat='identity', position = 'dodge2')
-
-
-grid.arrange(ratioEat, typeG, ncol=2)
+data <- read_csv(paste(dir, "/ainews.csv", sep = ""))

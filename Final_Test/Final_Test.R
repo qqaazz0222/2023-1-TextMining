6dba53277b6e9040459232741a69db7c395112dc
qqaazz0222 <qqaazz0222@naver.com>
2023-06-16 23:48:32 +0900

    추가 : txt 파일

--- Final_Test/Final_Test.R
+++ Final_Test/Final_Test.R
@@ -14,4 +14,8 @@ dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
 
 # ---------------------------------------------------------------------------------------------------------------------------------
 
+# 데이터 불러오기
 data <- read_csv(paste(dir, "/ainews.csv", sep = ""))
+
+# 텍스트 전처리
+preData <- data %>% str_replace_all("")

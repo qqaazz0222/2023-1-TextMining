a5993fd3864b753ac2fb4e2995e849c664df1a6a
qqaazz0222 <qqaazz0222@naver.com>
2023-06-16 23:31:51 +0900

    추가 : 크롤링 데이터(엑셀), 수정 : 크롤링 코드(마지막 페이지 )

--- Final_Test/__Crawling__.py
+++ Final_Test/__Crawling__.py
@@ -1,17 +1,26 @@
+# [ 라이브러리 ]
 import requests as rq
 from bs4 import BeautifulSoup
 import openpyxl
 import time
 
+# [ 엑셀로 저장하기 위한 워크북 생성 ]
 wb = openpyxl.Workbook()
+# [ 첫번째 시트 선택 ]
 ws = wb.active
+# [ A1셀에 title입력 ]
 ws['A1'] = 'title'
 
+# [ 뉴스 검색 키워드 설정 ]
 keyword = "인공지능"
-lastPageNo = 200
+# [ 검색 페이지 수 설정 (1페이지 당 10개의 뉴스) ]
+lastPageNo = 400
 
+# [ 초기 페이지 설정 ]
 pageNo = 0
+# [ 엑셀로 저장할 열 설정 ]
 rowNo = 2
+# [ 데이터 크롤링 후 엑셀에 저장 ]
 for articleNo in range(1, lastPageNo*10, 10):
     pageNo += 1
     print(f'=============={pageNo}페이지==============')
@@ -27,5 +36,5 @@ for articleNo in range(1, lastPageNo*10, 10):
         print(title)
         ws[f'A{rowNo}'] = title
         rowNo += 1
-
+# [ 엑셀 파일 저장 ]
 wb.save('./크롤링데이터.xlsx')

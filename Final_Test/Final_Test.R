43e0ed69c2ae43c1f80be4afa9c48fc0b7c933e5
qqaazz0222 <qqaazz0222@naver.com>
2023-06-16 23:55:37 +0900

    추가 : 단어 토큰화, 단어 빈도 분석

--- Final_Test/Final_Test.R
+++ Final_Test/Final_Test.R
@@ -9,6 +9,7 @@ library(dplyr)
 library(readr)
 library(stringr)
 library(ggplot2)
+library(tidytext)
 # ---------------------------------------------------------------------------------------------------------------------------------
 
 dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
@@ -20,3 +21,10 @@ ai_news <- readLines(paste(dir, "/ainews.txt", sep = ""), encoding = "UTF-8")
 
 # 텍스트 전처리 (불필요 문자 제거, 연속된 공백 제거, tibble 구조로 변경)
 new <- ai_news %>% str_replace_all("[^가-힣]", replacement = " ") %>% str_squish() %>% as_tibble()
+
+# 토큰화(단어)
+word_space <- new %>% unnest_tokens(input = value, output = word, token = "words")
+# 단어 빈도 구하기
+word_space <- word_space %>% count(word, sort = T)
+
+

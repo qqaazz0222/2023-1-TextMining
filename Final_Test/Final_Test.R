c3491ff4a7ff21266e49a2894e77a65484bdb62a
qqaazz0222 <qqaazz0222@naver.com>
2023-06-18 21:33:22 +0900

    추가 : 전처리 및 빈도분석

--- Final_Test/Final_Test.R
+++ Final_Test/Final_Test.R
@@ -25,3 +25,35 @@ font_add_google(name = "Black Han Sans" , family = "bhs")
 showtext_auto()
 
 # ---------------------------------------------------------------------------------------------------------------------------------
+
+# 데이터 불러오기
+google <- readLines(paste(dir, "/google_keynote.txt", sep = ""), encoding = "UTF-8")
+apple <- readLines(paste(dir, "/apple_wwdc.txt", sep = ""), encoding = "UTF-8")
+
+# 텍스트 전처리 (불필요 문자 제거, 연속된 공백 제거, tibble 구조로 변경)
+new_google <- google %>% str_replace_all("[^가-힣]", replacement = " ") %>% str_squish() %>% as_tibble()
+new_apple <- apple %>% str_replace_all("[^가-힣]", replacement = " ") %>% str_squish() %>% as_tibble()
+
+# 토큰화(단어)
+word_google <- new_google %>% unnest_tokens(input = value, output = word, token = extractNoun)
+word_apple <- new_apple %>% unnest_tokens(input = value, output = word, token = extractNoun)
+
+# 단어 빈도 구하기, 단어 길이 1 보다 긴 행만 추출
+word_google <- word_google %>% count(word, sort = T)  %>% filter(str_count(word)>1)
+word_apple <- word_apple %>% count(word, sort = T)  %>% filter(str_count(word)>1)
+top_20_google <- word_google %>% head(20)
+top_20_apple <- word_apple %>% head(20)
+
+ggplot(head(top_20_google), aes(x = reorder(word, n), y= n)) + 
+  geom_col() + 
+  coord_flip() +
+  geom_text(aes(label = n), hjust = -0.1) +
+  labs(x = NULL) +
+  theme(text = element_text(family = "ng"))
+
+ggplot(head(top_20_apple), aes(x = reorder(word, n), y= n)) + 
+  geom_col() + 
+  coord_flip() +
+  geom_text(aes(label = n), hjust = -0.1) +
+  labs(x = NULL) +
+  theme(text = element_text(family = "ng"))
